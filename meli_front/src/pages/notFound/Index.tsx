2be59741d5469ddd54c8a6055a16f11aff108c2e
Cristian Michel Galluccio <gallucciocristian@gmail.com>
2022-07-12 20:59:44 -0300

    Frontend: NotFoundPage added

--- meli_front/src/pages/notFound/Index.tsx
+++ meli_front/src/pages/notFound/Index.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
 
 export const NotFoundPage = () => {
+
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(`/`);
+}
+
   return (
-    <>
-        <div>NotFoundPage</div>
-    </>
+    <div className="not-found-container">
+        <h1>Parece que la página no existe</h1>
+        <br/>
+        <span onClick={handleRedirect}>Ir a la pagina principal</span>
+    </div>
   )
 }
